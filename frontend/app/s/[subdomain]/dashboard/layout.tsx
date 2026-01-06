c87b807e0fa1dd2ef02088f46effb41f1cda057b
Ibrahim Khalil <126571880+devsmaxcode@users.noreply.github.com>
2026-01-06 22:50:01 +0600

    move dashboard route to other path

--- frontend/app/s/[subdomain]/dashboard/layout.tsx
+++
@@ -1,5 +0,0 @@
-import DashboardLayout from '@/components/dashboard/dashboard-layout'
-
-export default function layout({ children }: { children: React.ReactNode }) {
-  return <DashboardLayout> {children} </DashboardLayout>
-}
